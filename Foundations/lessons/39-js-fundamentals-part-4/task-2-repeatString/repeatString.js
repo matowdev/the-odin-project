292e235965d053d9410677576fb2b187fc12d4dc
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-22 12:49:17 +0300

    [LES-39/cont] js-fund.. task 2 of 7
    Working with a string. Function/cycle. 7 tests.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-2-repeatString/repeatString.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-2-repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
+  let repeatStr = '';
 
+  if (count < 0) {
+    return (repeatStr = 'ERROR');
+  } else {
+    for (let i = 1; i <= count; i++) {
+      repeatStr += str;
+    }
+  }
+
+  return repeatStr;
 };
+
+console.log(repeatString('hey', 3)); // heyheyhey
+console.log(repeatString('hey', 10)); // heyheyheyheyheyheyheyheyheyhey
+console.log(repeatString('hey', 1)); // hey
+console.log(repeatString('hey', 0)); // ' '
+console.log(repeatString('hey', -1)); // ERROR
+console.log(repeatString('hey', randomNum())); // heyheyheyhey..
+
+function randomNum() {
+  let num = Math.floor(Math.random() * 100);
+  return num;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
