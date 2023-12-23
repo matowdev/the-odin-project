9d7eb91dfc75392bd2d0432099c483f4ba99aa29
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-23 14:27:03 +0300

    [LES-39/cont] js-fund.. task 3 of 7
    String revers. Function, multiple sequential methods. 4 tests.
    
    Worth noting:
      - the continued excitement of working with tests.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-3-reverseString/reverseString.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-3-reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let newStr = str.split('').reverse().join('');
+  return newStr;
 };
+
+console.log(reverseString('hello')); // olleh
+console.log(reverseString('hello there')); // ereht olleh
+console.log(reverseString('123! abc!')); // !cba !321
+console.log(reverseString('')); // ' '
 
 // Do not edit below this line
 module.exports = reverseString;
