c20bbcdd670e1483247016161c5671c642f99af7
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-24 14:43:09 +0300

    [LES-39/cont] js-fund.. task 4 of 7
    Working with arr's (convers. to arr). Func, match/excep. checking. 7 ts.
    
    Worth noting:
      - long deliberation.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-4-removeFromArray/removeFromArray.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-4-removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...data) {
+  let newArr = [...arr];
+  let dataArr = [...data];
 
+  for (let i = 0; i < dataArr.length; i++) {
+    let removeIndex = newArr.indexOf(dataArr[i]); // index №.. or -1
+
+    if (removeIndex !== -1) {
+      newArr.splice(removeIndex, 1);
+    }
+  }
+
+  return newArr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3)); // [1, 2, 4]
+console.log(removeFromArray([1, 2, 3, 4], 3, 2)); // [1, 4]
+console.log(removeFromArray([1, 2, 3, 4], 7, 'tacos')); // [1, 2, 3, 4]
+console.log(removeFromArray([1, 2, 3, 4], 7, 2)); // [1, 3, 4]
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)); // []
+console.log(removeFromArray(['hey', 2, 3, 'ho'], 'hey', 3)); // [2, 'ho']
+console.log(removeFromArray([1, 2, 3], '1', 3)); // [1, 2]
 
 // Do not edit below this line
 module.exports = removeFromArray;
