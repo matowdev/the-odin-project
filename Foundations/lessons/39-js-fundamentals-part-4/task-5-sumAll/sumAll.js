e97dcada54657fc1a5df09a60ef2300c3d1bb8a4
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-25 13:35:33 +0300

    [LES-39/cont] js-fund.. task 5 of 7
    Working with natural num's. Func, output their sum (1+2..). 6 tests.
    
    Worth noting:
      - TOP's solution.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-5-sumAll/sumAll.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-5-sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (n, m) {
+  if (n === Number(n) && n > 0 && m === Number(m) && m > 0) {
+    let numMax = Math.max(n, m);
+    let arrLength = new Array(numMax);
+    let arrSum = 0;
 
+    for (let i = 1; i <= arrLength.length; i++) {
+      arrSum += i;
+    }
+
+    return arrSum;
+  } else {
+    return 'ERROR';
+  }
 };
+
+console.log(sumAll(1, 4)); // 10
+console.log(sumAll(1, 4000)); // 8002000
+console.log(sumAll(123, 1)); // 7626
+console.log(sumAll(-10, 4)); // ERROR
+console.log(sumAll(10, '90')); // ERROR
+console.log(sumAll(10, [90, 1])); // ERROR
 
 // Do not edit below this line
 module.exports = sumAll;
