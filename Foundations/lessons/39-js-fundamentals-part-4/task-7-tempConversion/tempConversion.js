1e42f7c6c453167bef4a79821eb82913834c1829
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-25 17:00:52 +0300

    [LES-39/compl] js-fund.. task 7 of 7
    Converting the resulting temperatures, Fahrenheit/Celsius. 6 tests.
    
    Worth noting:
      - conversion formulas.
      - that was the last task for this lesson.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-7-tempConversion/tempConversion.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-7-tempConversion/tempConversion.js
@@ -1,11 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+
+  if (celsius !== 0 && !Number.isInteger(celsius)) {
+    celsius = Number(celsius.toFixed(1));
+  }
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32)); // 0
+console.log(convertToCelsius(100)); // 37.8
+console.log(convertToCelsius(-100)); // -73.3
+
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+
+  if (fahrenheit !== 0 && !Number.isInteger(fahrenheit)) {
+    fahrenheit = Number(fahrenheit.toFixed(1));
+  }
+
+  return fahrenheit;
 };
+
+console.log(convertToFahrenheit(0)); // 32
+console.log(convertToFahrenheit(73.2)); // 163.8
+console.log(convertToFahrenheit(-10)); // 14
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
