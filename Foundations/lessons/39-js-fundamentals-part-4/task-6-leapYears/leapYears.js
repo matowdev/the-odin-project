c188ddb012fb39bf19899c0c56862b53d914ef28
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-25 16:09:10 +0300

    [LES-39/cont] js-fund.. task 6 of 7
    Determining, is transferred year a leap year or not. 6 tests.
    
    Worth noting:
      - a number of conditions.
    
    Foundation(s) path

--- Foundations/lessons/39-js-fundamentals-part-4/task-6-leapYears/leapYears.js
+++ Foundations/lessons/39-js-fundamentals-part-4/task-6-leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 
+  if (isLeapYear) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(1996)); // true
+console.log(leapYears(1997)); // false
+console.log(leapYears(34992)); // true
+console.log(leapYears(1900)); // false
+console.log(leapYears(1600)); // true
+console.log(leapYears(700)); // false
 
 // Do not edit below this line
 module.exports = leapYears;
