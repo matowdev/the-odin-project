2504a2a1749862c3ccb09abe9888703e7acff469
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-28 11:55:25 +0300

    [PROJ-EAS/compl] Add some func's, btn's
    Organization of two drawing modes, switching. Cleaning the canvas.
    
    Worth noting:
      - no point in going deeper without understanding/knowledge.
    
    Foundation(s) path

--- Foundations/projects/etch-a-sketch/index.js
+++ Foundations/projects/etch-a-sketch/index.js
@@ -13,27 +13,62 @@ const colors = [
   '#A69E00',
   '#A368D5',
 ];
-const SQUARES_NUMBER = 500;
+const SQUARES_NUMBER = 992;
+let isDrawing = false;
+
+const changeBtn = document.querySelector('.change__btn');
+changeBtn.addEventListener('click', enableRainbowMode);
+
+function enableRainbowMode() {
+  isDrawing = false;
+  board.addEventListener('mousemove', setRandomColor);
+}
+
+const resetBtn = document.querySelector('.reset__btn');
+resetBtn.addEventListener('click', clearCanvas);
+
+function clearCanvas() {
+  isDrawing = false; // Добавьте эту строку
+  board.removeEventListener('mousemove', setRandomColor); // Добавьте эту строку
+
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.style.backgroundColor = '#2b6684';
+  });
+}
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   square.addEventListener('mouseover', setColor);
-  // square.addEventListener('mouseleave', removeColor); // что-бы цвет пропадал
   board.append(square);
 }
 
+board.addEventListener('mousedown', () => {
+  event.preventDefault();
+  isDrawing = true;
+});
+
+board.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+board.addEventListener('mouseleave', () => {
+  isDrawing = false;
+});
+
 function setColor(event) {
+  if (!isDrawing) return;
+  const element = event.target;
+  const color = '#29354d';
+  element.style.backgroundColor = color;
+}
+
+function setRandomColor(event) {
+  if (!isDrawing) return;
   const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-}
-
-function removeColor(event) {
-  const element = event.target;
-  element.style.backgroundColor = '#2b6684';
-  element.style.boxShadow = `0 0 2px #9cacbf`;
 }
 
 function getRandomColor() {
